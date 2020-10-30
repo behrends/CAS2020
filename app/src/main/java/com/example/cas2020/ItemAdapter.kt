21088633f31ebe33b6dc4064754994365138a62a
Erik Behrends <behrends@users.noreply.github.com>
2020-10-30 16:14:21 +0100

    ItemAdapter vorbereitet

---
+++ app/src/main/java/com/example/cas2020/ItemAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.cas2020
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
