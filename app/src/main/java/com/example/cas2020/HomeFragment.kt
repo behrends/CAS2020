4d85990a6cd26371d382fb321b07b698ff151528
Erik Behrends <behrends@users.noreply.github.com>
2020-10-30 14:52:05 +0100

    Button navigiert zum Detail Fragment

--- app/src/main/java/com/example/cas2020/HomeFragment.kt
+++ app/src/main/java/com/example/cas2020/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val detailsButton = view.findViewById<Button>(R.id.buttonDetails)
+        detailsButton.setOnClickListener { view: View ->
+            // TODO: navigiere zu DetailFragment
+            view.findNavController().navigate(R.id.action_homeFragment_to_detailsFragment2)
+        }
+        return view
     }
 }
