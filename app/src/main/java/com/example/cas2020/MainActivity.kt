12f1bd64553146a9ae0e0c0d90889e243d8bd4d2
Erik Behrends <behrends@users.noreply.github.com>
2020-10-30 14:05:49 +0100

    Single Activity Navigation mit HomeFragment

--- app/src/main/java/com/example/cas2020/MainActivity.kt
+++ app/src/main/java/com/example/cas2020/MainActivity.kt
@@ -2,23 +2,14 @@ package com.example.cas2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import com.example.cas2020.databinding.ActivityMainBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.button.setOnClickListener {
-            Toast.makeText(this, "OK HAT GEKLAPPT!", Toast.LENGTH_LONG).show()
-            binding.textView.text = "WURDE GEÄNDERT"
-        }
-
-        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
-            binding.textView.visibility = if(isChecked) View.INVISIBLE else View.VISIBLE
-        }
+        setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.nav_host_fragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
