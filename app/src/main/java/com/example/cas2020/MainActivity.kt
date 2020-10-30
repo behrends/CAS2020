407c5defefcbf1940f119f867e6ce3a6232cf2b8
Erik Behrends <behrends@users.noreply.github.com>
2020-10-30 11:17:38 +0100

    TextView durch Klick ändern

--- app/src/main/java/com/example/cas2020/MainActivity.kt
+++ app/src/main/java/com/example/cas2020/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cas2020
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +15,15 @@ class MainActivity : AppCompatActivity() {
         // bzw. das UI der Activity wird dargestellt
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textView)
 
-        // TODO: Button anklicken soll Info zeigen
+        // Button anklicken soll TextView  ändern
         // 1. Button-View-Element aus XML einer Konstanten zuweisen
         val button = findViewById<Button>(R.id.button)
         // 2. Button mit Klick-Handler ausstatten
         button.setOnClickListener {
             Toast.makeText(this, "OK HAT GEKLAPPT!", Toast.LENGTH_LONG).show()
+            textView.text = "WURDE GEÄNDERT"
         }
     }
 }
